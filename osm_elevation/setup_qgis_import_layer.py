9e56c3656bbf9dcca1bd7ebbe9ef86efb7cb5f26
martapienkowska <martucha_p@wp.pl>
2018-10-16 19:21:43 +0200

    Added QGIS plugin

---
+++ osm_elevation/setup_qgis_import_layer.py
@@ -0,0 +1,36 @@
+from PyQt4.QtGui import QInputDialog
+def docstring():
+	"""Import table with altitude labels to QGIS."""
+	
+help(docstring)
+
+uri = QgsDataSourceURI()
+
+host, rest = QInputDialog.getText(None, " ", "insert host")
+port, rest = QInputDialog.getText(None, " ", "insert port")
+db_name, rest = QInputDialog.getText(None, " ", "insert db_name")
+user, rest = QInputDialog.getText(None, " ", "insert user")
+label_size, rest = QInputDialog.getText(None, " ", "insert size of label")
+
+
+uri.setConnection(host, port, db_name, user, "")
+uri.setDataSource("public", "osm_nmt_altitude", "geom", "")
+vl_alt = QgsVectorLayer(uri.uri(), "osm_nmt_altitude", "postgres")
+if not vl_alt.isValid():
+    print "vector layer not valid"
+    
+my_renderer = vl_alt.rendererV2()
+my_renderer.symbol().setSize(1)
+vl_alt.triggerRepaint()
+
+label = QgsPalLayerSettings()
+label.readFromLayer(vl_alt)
+label.enabled = True
+label.fieldName = "h"
+label.placement= QgsPalLayerSettings.AroundPoint
+label.setDataDefinedProperty(QgsPalLayerSettings.Size,True,True,label_size,"")
+label.writeToLayer(vl_alt)
+
+QgsMapLayerRegistry.instance().addMapLayers([vl_alt])
+
+
